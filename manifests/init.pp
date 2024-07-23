ca6d4a78f1d98ccc250ae2ac79463054c737f306
Phong Tran <phongttran01@gmail.com>
2024-07-23 10:16:42 -0500

    Add postgresql module

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,4 +9,20 @@ class profile_pgbackrest (
   include stdlib
   include profile_pgbackrest::yumrepos
   include profile_pgbackrest::config
+  include postgresql::server
+
+  # Configuration parameters for the PostgreSQL server
+  class { 'postgresql::server':
+    version           => '14',               # Specify the version of PostgreSQL
+    listen_addresses  => '*',                # Listen on all available IP addresses
+    port              => 5432,               # Default PostgreSQL port
+    manage_firewall   => true,               # Manage firewall rules to allow traffic on PostgreSQL port
+    postgres_password => lookup('postgresql::server::dbpassword'),   # Password for the PostgreSQL superuser
+  }
+
+  # Create a new PostgreSQL database owned by the default 'postgres' user
+  postgresql::server::db { 'test_database':
+    user     => 'postgres',        # Default PostgreSQL superuser
+    password => postgresql_password('postgres', lookup('postgresql::server::dbpassword')), # Encrypted password for the user
+  }
 }
--- manifests/yumrepos.pp
+++ manifests/yumrepos.pp
@@ -9,14 +9,6 @@ class profile_pgbackrest::yumrepos () {
         gpgkey        => lookup('profile_pgbackrest::yumrepo_gpgkey'),
         repo_gpgcheck => true,
       }
-
-      # yum_repo { 'yum_15_redhat':
-      #   descr         => 'yum_15_redhat',
-      #   baseurl       => lookup('profile_pgbackrest::yum_15_redhat_url'),
-      #   gpgcheck      => true,
-      #   gpgkey        => lookup('profile_pgbackrest::yumrepo_gpgkey'),
-      #   repo_gpgcheck => true,
-      # }
     }
   }
 }
