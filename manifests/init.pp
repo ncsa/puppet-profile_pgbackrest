cab0a25bcc65982b7c719425dfccf32f84449feb
Phong Tran <phongttran01@gmail.com>
2024-10-30 14:00:18 -0500

    SVCPLAN-7454 - Remove backup.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,13 @@
+# @summary Configure profile_pgbackrest
+#
+# @example
 #   include profile_pgbackrest
 class profile_pgbackrest (
   # PARAMETERS
-  String $postgresql_data,
-  String $backup_path,
 ) {
-# SUB-MODULES TO INCLUDE 
+  # SUB-MODULES TO INCLUDE 
   include epel
   include stdlib
   include profile_pgbackrest::yumrepos
   include profile_pgbackrest::config
-
-  # Configuration parameters for the PostgreSQL server
-  class { 'postgresql::server':
-    version           => '14',               # Specify the version of PostgreSQL
-    listen_addresses  => '*',                # Listen on all available IP addresses
-    port              => 5432,               # Default PostgreSQL port
-  }
 }
