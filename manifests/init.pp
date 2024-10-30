ed1963ff4a9e499c7690b6b7d0400b0902e34486
Phong Tran <phongttran01@gmail.com>
2024-10-30 14:21:36 -0500

    Removed postgresql config

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,9 +11,9 @@ class profile_pgbackrest (
   include profile_pgbackrest::config
 
   # Configuration parameters for the PostgreSQL server
-  class { 'postgresql::server':
-    version           => '14',               # Specify the version of PostgreSQL
-    listen_addresses  => '*',                # Listen on all available IP addresses
-    port              => 5432,               # Default PostgreSQL port
-  }
+#  class { 'postgresql::server':
+#    version           => '14',               # Specify the version of PostgreSQL
+#    listen_addresses  => '*',                # Listen on all available IP addresses
+#    port              => 5432,               # Default PostgreSQL port
+#  }
 }
